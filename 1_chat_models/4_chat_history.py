b951c72c1db9df69f14207bb252cb01af6e28d98
Suyogly <bizpurpose09@gmail.com>
2025-07-14 10:11:52 +0545

    chat history

--- 1_chat_models/4_chat_history.py
+++ 1_chat_models/4_chat_history.py
@@ -1,3 +1,26 @@
-# later
+from langchain_groq import ChatGroq
+from langchain_core.messages import HumanMessage, SystemMessage
+from dotenv import load_dotenv
 
+llm = ChatGroq(
+    model="llama-3.3-70b-versatile",
+    max_tokens=None
+)
+
+chat_history = []
+
+system_message = SystemMessage("You are a helpful businessman who has made 5 businesses to multi-million dollar company all by yourself and you know how things work behind the scenes.")
+chat_history.append(system_message)
+
+while True:
+    query = input("You: \n")
+    if query.lower() == "exit":
+        break
+    chat_history.append(HumanMessage(content=query))
+
+    result = llm.invoke(chat_history)
+    response = result.content
+
+    chat_history.append(response)
+    print(f"AI: {response} \n")
 
