b82823d436b41ad19d03db6cdc8029d073f3511c
Suyogly <bizpurpose09@gmail.com>
2025-07-22 17:45:53 +0545

    text loader + llm

--- 4_document_loaders/4_1_text_loader.py
+++ 4_document_loaders/4_1_text_loader.py
@@ -1,6 +1,31 @@
 from langchain_community.document_loaders import TextLoader
+from langchain_groq import ChatGroq
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from dotenv import load_dotenv
 
-loader = TextLoader("files/hello.txt")
+load_dotenv()
 
-result = loader.load()
+llm1 = ChatGroq(
+    model="llama-3.3-70b-versatile",
+    max_tokens=None
+)
+
+# load text files
+txt_loader = TextLoader("files/hello.txt")
+
+text_content = txt_loader.load()
+print(text_content)
+print("\n ----------------- \n")
+
+prompt1 = PromptTemplate(
+    template="check what's there in the {text}, and tell me the gist about it.",
+    input_variables=["text"]
+)
+
+parser = StrOutputParser()
+
+chain = prompt1 | llm1 | parser
+
+result = chain.invoke({"text" : text_content})
 print(result)
