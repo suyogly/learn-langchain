c774b6fff9681670e76951885e59a7ed9cde49e3
Suyogly <bizpurpose09@gmail.com>
2025-08-02 11:06:17 +0545

    pdf loader

---
+++ 4_document_loaders/4_3_pdf_loader.py
@@ -0,0 +1,31 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from dotenv import load_dotenv
+
+load_dotenv()
+
+pdf = PyPDFLoader(file_path="files/jim-collins-beyond-entrepreneurship.pdf")
+content = pdf.load()
+
+llm = ChatGoogleGenerativeAI(
+    model="gemini-2.0-flash",
+    max_tokens=None
+)
+
+prompt = PromptTemplate(
+    template="from the {pdf}, please let me know about this {topic}",
+    input_variables=["pdf", "topic"]
+)
+
+parser = StrOutputParser()
+
+chain = prompt | llm | parser
+
+result = chain.invoke({
+    "pdf" : content,
+    "topic": "what does he talk about in chapter 2"
+})
+
+print(result)
