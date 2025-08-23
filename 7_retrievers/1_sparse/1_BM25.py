04991cd253b112eb8c1527c1b891192686951e43
Suyogly <bizpurpose09@gmail.com>
2025-08-23 22:51:21 +0545

    still doin bm25

--- 7_retrievers/1_sparse/1_BM25.py
+++ 7_retrievers/1_sparse/1_BM25.py
@@ -3,7 +3,7 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_chroma import Chroma
+# from langchain_chroma import Chroma (as we dont use vector stores in sparse retrieval)
 from langchain_community.retrievers import BM25Retriever
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_core.documents import Document
@@ -14,6 +14,19 @@ pdf = PyPDFLoader(file_path="files/jim-collins-beyond-entrepreneurship.pdf")
 doc = pdf.load()
 # print(doc)
 
-chunk = BM25Retriever(
-
+# chunk the document with text splitters
+splitter = RecursiveCharacterTextSplitter(
+    chunk_size = 500,
+    chunk_overlap = 100
 )
+
+splitted_content = splitter.split_documents(doc)
+# print(splitted_content)
+
+# define retriever(BM25)
+retriever = BM25Retriever.from_documents(
+    documents=splitted_content,
+    k=2
+)
+
+retriever.invoke()
