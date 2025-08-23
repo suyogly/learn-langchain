351b1d5b26c319ac447725ddef2cb46db676b8c8
Suyogly <bizpurpose09@gmail.com>
2025-08-23 23:33:18 +0545

    bm25 finished

--- 7_retrievers/1_sparse/1_BM25.py
+++ 7_retrievers/1_sparse/1_BM25.py
@@ -1,12 +1,16 @@
 # this is the traditional keyword matching retrieval method called BM25
 
 from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 # from langchain_chroma import Chroma (as we dont use vector stores in sparse retrieval)
 from langchain_community.retrievers import BM25Retriever
 from langchain_community.document_loaders import PyPDFLoader
-from langchain_core.documents import Document
+from langchain_core.prompts import ChatPromptTemplate
+from langchain.schema.runnable import RunnablePassthrough
+from langchain_core.output_parsers import StrOutputParser
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # load the document
 
@@ -26,7 +30,37 @@ splitted_content = splitter.split_documents(doc)
 # define retriever(BM25)
 retriever = BM25Retriever.from_documents(
     documents=splitted_content,
-    k=2
+    k=4
 )
 
-retriever.invoke()
+llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash", max_tokens=None)
+
+prompt = ChatPromptTemplate.from_template("""
+Answer the question based on the following context:
+
+Context: {context}
+
+Question: {question}
+
+Answer:
+""")
+
+parser = StrOutputParser()
+
+def join_doc(docs):
+    return "\n\n".join([doc.page_content for doc in docs])
+
+
+chain = (
+    {
+        "context": retriever | join_doc,
+        "question": RunnablePassthrough()
+    }
+    | prompt
+    | llm
+    | parser
+)
+
+result = chain.invoke("how to get rich")
+
+print(result)
