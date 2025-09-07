19d2ce44c6acbec59152c1f35129cf8ecb788cf2
Suyogly <bizpurpose09@gmail.com>
2025-09-07 21:24:37 +0545

    fixed tooling test with Tavily Search

--- 8_tools/1_tools_test.py
+++ 8_tools/1_tools_test.py
@@ -1,5 +1,25 @@
-from langchain_community.tools import DuckDuckGoSearchRun
+from langchain_community.tools import TavilySearchResults
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain.schema.runnable import RunnablePassthrough
+from dotenv import load_dotenv
 
-search = DuckDuckGoSearchRun()
-search_result = search.invoke("Capital of Nepal")
-print(search_result)
+load_dotenv()
+
+search = TavilySearchResults(kwargs=4)
+
+llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash", max_tokens=None)
+parser = StrOutputParser()
+
+prompt = ChatPromptTemplate.from_template(
+    "Based on these search results about '{query}', provide a summary:\n\n{search_results}"
+)
+
+chain = {
+    "query": RunnablePassthrough(),
+    "search_results": search
+} | prompt | llm | parser
+
+result = chain.invoke("social media bans in nepal")
+print(result)
